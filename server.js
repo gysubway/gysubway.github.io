4c53b0ec8a5a215b0679b2ebe0824f1d9a028dea
gysubway <15910259721@163.com>
2026-08-16 21:49:46 +0800

    1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
